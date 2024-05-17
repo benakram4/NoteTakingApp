2da8d9cfdb4ecce4b8d8c119898220aa78dcf8b5
Gazal100 <ggarg4@myseneca.ca>
2024-05-17 18:00:23 -0400

    Update note-service.service.ts

--- src/app/note-service.service.ts
+++ src/app/note-service.service.ts
@@ -1,9 +1,37 @@
 import { Injectable } from '@angular/core';
+import { MOCK_NOTES } from '../mock/notes';
+import { Note } from './models/note.model';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class NoteServiceService {
 
+  private notes = MOCK_NOTES;
+
   constructor() { }
+
+  // Get all notes
+  getNotes(): Note[] {
+    return this.notes;
+  }
+
+  // Get note by ID
+  getNoteById(id: number): Note | undefined {
+    return this.notes.find(note => note.id === id);
+  }
+
+  // Delete note by ID
+  deleteNoteById(id: number): void {
+    this.notes = this.notes.filter(note => note.id !== id);
+  }
+
+  // Update note
+  updateNote(updatedNote: Note): void {
+    const index = this.notes.findIndex(note => note.id === updatedNote.id);
+    if (index !== -1) {
+      this.notes[index] = updatedNote;
+    }
+  }
 }
